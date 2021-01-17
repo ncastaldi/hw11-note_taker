2b2e60169875e9439c16cdfa98d5e8526b7c10ea
Nathan Castaldi <nathan@castaldi.dev>
2021-01-17 18:09:37 -0500

    creatd notes route; tested working locally

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = 3000;
+const port = 3030;
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(port, () => {
