6432c6d27c7f20458581e70a2fb8541c61f9a375
Nathan Castaldi <nathan@castaldi.dev>
2021-01-17 18:27:16 -0500

    changed listening port to 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
