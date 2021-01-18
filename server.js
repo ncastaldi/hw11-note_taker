8a6bdefcc7aace313005fc24ca1bfa950abb7ecd
Nathan Castaldi <nathan@castaldi.dev>
2021-01-18 10:03:44 -0500

    added post route to store new notes

--- server.js
+++ server.js
@@ -15,10 +15,61 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
 // API Routes
+
+// Get Route to read and display from JSON db
 app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
     console.log(res);
 });
+
+// Post Route to save new note to JSON db
+app.post("/api/notes", function (req, res) {
+    // Declare empty array for current and new notes
+    let savedNotes = [];
+
+    // Read currently stored notes list
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        // Parse currently stored notes
+        const currentNoteData = JSON.parse(data);
+
+        for (let i = 0; i < currentNoteData.length; i++) {
+            // Create new note object for each current note
+            const note = {
+                title: currentNoteData[i].title,
+                text: currentNoteData[i].text,
+                id: i,
+            };
+
+            // Add note object to array
+            savedNotes.push(note);
+        }
+
+        // Create new note object for new note
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: savedNotes.length,
+        };
+
+        // Push new note into array
+        savedNotes.push(newNote);
+
+        // Stringify savedNotes array
+        savedNotes = JSON.stringify(savedNotes);
+
+        fs.writeFile("./db/db.json", savedNotes, (err) => {
+            if (err) throw err;
+            console.log("The file has been saved!");
+        });
+
+        // Send a response to resolve the post request
+        res.send("your note has been saved");
+    });
+});
+
+
 // API Routes
 
 // HTML Routes
