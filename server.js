9f27071c7f91b98446e217e82ec23a2f997036da
Nathan Castaldi <nathan@castaldi.dev>
2021-01-18 09:21:12 -0500

    updated server.js comments

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+// Include required dependencies
 const express = require("express");
 const app = express();
 const path = require("path");
 
+// Declare port
 const PORT = process.env.PORT || 8080;
 
+// API Routes
+
+// API Routes
+
+// HTML Routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+// HTML Routes
 
+// Enable Express instance to listen on defined port(s)
 app.listen(PORT, () => {
     console.log("Server listening on port: " + PORT);
 })
