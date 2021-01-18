5deacf34d32a6a2046a9cf1ac1afb1eb7a136f1f
Nathan Castaldi <nathan@castaldi.dev>
2021-01-18 09:41:12 -0500

    added middleware to read and display data from json file

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const path = require("path");
 // Declare port
 const PORT = process.env.PORT || 8080;
 
+// Create middleware to parse the request body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set the web root to the public folder
+app.use(express.static(__dirname + "/public"));
+
 // API Routes
 app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+    console.log(res);
 });
 // API Routes
 
@@ -25,4 +34,4 @@ app.get("*", function (req, res) {
 // Enable Express instance to listen on defined port(s)
 app.listen(PORT, () => {
     console.log("Server listening on port: " + PORT);
-})
+});
